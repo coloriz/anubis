562b7e8697712f8af76df03c76447661346caef8
Jimmy <nunu3041@gmail.com>
2021-11-17 06:06:48 +0900

    Use sqlite db on debug

--- src/anubis/settings.py
+++ src/anubis/settings.py
@@ -88,6 +88,12 @@ DATABASES = {
     }
 }
 
+if DEBUG:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
